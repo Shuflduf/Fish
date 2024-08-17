cdf3c9703ce17a69dd187998d6adb20d860677d6
Shuflduf <shuflduf@gmail.com>
2024-08-17 05:10:48 -0600

    hold to cast further

--- main.gd
+++ main.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
-func _ready() -> void:
-	cast()
-
+func _physics_process(delta: float) -> void:
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		$Node2D/rod_hint.position.x += delta * 300
+	elif Input.is_action_just_released("cast"):
+		cast()
+	
 
 func cast():
 	var tween = get_tree().create_tween()
-	tween.tween_property($player/rod, "position", Vector2(500, 150), 0.6)
-	tween.finished.connect(func(): 
-		$player/rod.position = Vector2.ZERO)
+	tween.tween_property($player/rod, "position", $player.to_local(%rod_hint.global_position), 0.6)
+	%rod_hint.position.x = 0
+	await tween.finished
+	await get_tree().create_timer(1).timeout
+	$player/rod.position = Vector2.ZERO
