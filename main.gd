b8e26782cfa9ed660d55292b2c9a2cd55e8c961b
Shuflduf <shuflduf@gmail.com>
2024-08-17 06:12:33 -0600

    bare bones shop system

--- main.gd
+++ main.gd
@@ -4,6 +4,9 @@ var fishing = false
 
 signal caught
 
+# in percent, where 0% is instant and 100% is normal
+var cast_speed = 100.0
+
 func _physics_process(delta: float) -> void:
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		%rod_hint.position.x += delta * 300
@@ -19,7 +22,9 @@ func cast():
 		return
 	fishing = true
 	
-	await launch_into(%rod, %rod_hint.global_position)
+	var pre_cast_speed = 0.6 * (cast_speed / 100.0)
+	await launch_into(%rod, %rod_hint.global_position, 0, pre_cast_speed)
+	
 	%rod_hint.position.x = 0
 	await get_tree().create_timer(randf_range(1, 5)).timeout
 	var new_fish = $fish.duplicate()
@@ -31,7 +36,7 @@ func cast():
 	$player/rod.position = Vector2.ZERO
 	fishing = false
 
-func launch_into(object: Node2D, target: Vector2, offset := 0.0):
+func launch_into(object: Node2D, target: Vector2, offset := 0.0, speed = 0.6):
 	var ver_tween = get_tree().create_tween()\
 			.set_trans(Tween.TRANS_SINE)
 	ver_tween.tween_property(object, "position:y", \
--- ui.gd
+++ ui.gd
@@ -8,3 +8,17 @@ func _ready() -> void:
 func add_fish():
 	fish += 1
 	$Control/Fish.text = "Fish: " + str(fish)
+
+
+func _on_button_pressed() -> void:
+	if get_parent().cast_speed - 5 > 0:
+		
+		$Window/VBoxContainer/FasterCast/Label.text = \
+				"Faster Cast: " \
+				+ str((1.0 / get_parent().cast_speed) * 1000).pad_decimals(2) \
+				+ "%"
+		get_parent().cast_speed -= 5.0
+
+
+func _on_window_close_requested() -> void:
+	$Window.hide()
