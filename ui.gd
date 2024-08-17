5d7a4be0ba478ee1c496b54993c6253a6493a36b
Shuflduf <shuflduf@gmail.com>
2024-08-17 06:40:49 -0600

    exports

--- ui.gd
+++ ui.gd
@@ -14,6 +14,10 @@ func _on_window_close_requested() -> void:
 
 
 func _on_cast_button_pressed() -> void:
+	
+	if fish < 10:
+		return
+
 	if get_parent().cast_speed + 3.0 <= 90:
 		get_parent().cast_speed += 3.0
 		$Window/VBoxContainer/FasterCast/CastLabel.text = \
@@ -23,9 +27,17 @@ func _on_cast_button_pressed() -> void:
 
 
 func _on_bait_button_pressed() -> void:
-	if get_parent().bait_quality <= 80:
+	
+	if fish < 30:
+		return
+	
+	if get_parent().bait_quality + 4.0 <= 80:
 		get_parent().bait_quality += 4.0
 		$Window/VBoxContainer/BetterBait/BaitLabel.text = \
 				"Better Bait: " \
 				+ str(get_parent().bait_quality) \
 				+ "%"
+
+
+func _on_button_pressed() -> void:
+	$Window.show()
